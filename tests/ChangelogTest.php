f7ebf4f601e9d122138108c7074d3815be95c253
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-06-08 20:48:28 +0200

    ISSUE #665: Add test for changelog

---
+++ tests/ChangelogTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Domain\App\AppVersion;
+use PHPUnit\Framework\TestCase;
+
+class ChangelogTest extends TestCase
+{
+    public function testThatChangelogContainsLatestVersion(): void
+    {
+        $changelog = file_get_contents(__DIR__.'/../CHANGELOG.md');
+
+        $latestVersion = AppVersion::getSemanticVersion();
+        $this->assertStringContainsString(sprintf('# [%s]', $latestVersion), $changelog);
+    }
+}
