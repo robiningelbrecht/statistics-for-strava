b15c08e6134baf7e84b32864e33f3586ec4b5932
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-07-04 14:45:06 +0200

    ISSUE #818: SvgsTwigExtension

--- tests/Domain/Strava/SportTypeTest.php
+++ tests/Domain/Strava/SportTypeTest.php
@@ -6,23 +6,10 @@ use App\Domain\Strava\Activity\SportType\SportType;
 use App\Tests\ContainerTestCase;
 use Spatie\Snapshots\MatchesSnapshots;
 use Symfony\Contracts\Translation\TranslatorInterface;
-use Twig\Environment;
 
 class SportTypeTest extends ContainerTestCase
 {
     use MatchesSnapshots;
-
-    public function testGetSvgIcon(): void
-    {
-        /** @var Environment $twig */
-        $twig = $this->getContainer()->get(Environment::class);
-
-        $snapshot = [];
-        foreach (SportType::cases() as $sportType) {
-            $snapshot[] = $twig->load('svg/sport-type/svg-'.$sportType->getSvgIcon().'.html.twig')->render();
-        }
-        $this->assertMatchesHtmlSnapshot(implode(PHP_EOL, $snapshot));
-    }
 
     public function testGetTemplateName(): void
     {
