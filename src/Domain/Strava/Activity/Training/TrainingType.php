c03d9b101ae2696cd17e7f5a40d9f4da313398de
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-07-07 21:51:42 +0200

    ISSUE #764: Fix TimeInZones

--- src/Domain/Strava/Activity/Training/TrainingType.php
+++ src/Domain/Strava/Activity/Training/TrainingType.php
@@ -4,12 +4,27 @@ declare(strict_types=1);
 
 namespace App\Domain\Strava\Activity\Training;
 
-enum TrainingType: string
+use Symfony\Contracts\Translation\TranslatableInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+enum TrainingType implements TranslatableInterface
 {
-    case POLARIZED = 'Polarized';
-    case PYRAMIDAL = 'Pyramidal';
-    case THRESHOLD = 'Threshold';
-    case HIIT = 'HIIT';
-    case BASE = 'Base';
-    case UNIQUE_OTHER = 'Unique/Other';
+    case POLARIZED;
+    case PYRAMIDAL;
+    case THRESHOLD;
+    case HIIT;
+    case BASE;
+    case UNIQUE_OTHER;
+
+    public function trans(TranslatorInterface $translator, ?string $locale = null): string
+    {
+        return match ($this) {
+            self::POLARIZED => $translator->trans('Polarized', locale: $locale),
+            self::PYRAMIDAL => $translator->trans('Pyramidal', locale: $locale),
+            self::THRESHOLD => $translator->trans('Threshold', locale: $locale),
+            self::HIIT => $translator->trans('HIIT', locale: $locale),
+            self::BASE => $translator->trans('Base', locale: $locale),
+            self::UNIQUE_OTHER => $translator->trans('Unique/other', locale: $locale),
+        };
+    }
 }
