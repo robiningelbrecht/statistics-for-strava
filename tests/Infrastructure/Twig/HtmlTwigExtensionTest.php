414192f70f73893cb4253de1608960d710543159
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-08-07 19:13:23 +0200

    ISSUE #957: Make sure tab IDs are unique for all dashboard widgets

---
+++ tests/Infrastructure/Twig/HtmlTwigExtensionTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests\Infrastructure\Twig;
+
+use App\Infrastructure\Twig\HtmlTwigExtension;
+use PHPUnit\Framework\TestCase;
+
+class HtmlTwigExtensionTest extends TestCase
+{
+    public function testCleanUniqueId(): void
+    {
+        $twigExtension = new HtmlTwigExtension();
+        $id = 'test';
+
+        $this->assertEquals(
+            '--1',
+            $twigExtension->uniqueNumberForId($id)
+        );
+        $this->assertEquals(
+            '--2',
+            $twigExtension->uniqueNumberForId($id)
+        );
+    }
+}
