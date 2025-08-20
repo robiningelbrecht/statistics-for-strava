3033a502f86bc0ee85d34c3d658ff3f5e04bf453
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-08-20 08:17:04 +0200

    ISSUE #1012: Update widget docs

--- src/Domain/Calendar/MonthlyStats/MonthlyStatsContext.php
+++ src/Domain/Calendar/MonthlyStats/MonthlyStatsContext.php
@@ -6,7 +6,15 @@ namespace App\Domain\Calendar\MonthlyStats;
 
 enum MonthlyStatsContext: string
 {
-    case MOVING_TIME = 'time';
+    case MOVING_TIME = 'movingTime';
     case DISTANCE = 'distance';
     case ELEVATION = 'elevation';
+
+    public function getUrlSlug(): string
+    {
+        return match ($this) {
+            MonthlyStatsContext::MOVING_TIME => 'time',
+            default => $this->value,
+        };
+    }
 }
