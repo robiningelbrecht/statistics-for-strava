aa792636a3e9aa04fb7a9f625322a82d9ee27d37
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-08-20 08:23:23 +0200

    ISSUE #1012: Improve monthly widget title

--- src/Domain/Calendar/MonthlyStats/MonthlyStatsContext.php
+++ src/Domain/Calendar/MonthlyStats/MonthlyStatsContext.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Domain\Calendar\MonthlyStats;
 
-enum MonthlyStatsContext: string
+use Symfony\Contracts\Translation\TranslatableInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+enum MonthlyStatsContext: string implements TranslatableInterface
 {
     case MOVING_TIME = 'movingTime';
     case DISTANCE = 'distance';
@@ -15,6 +18,15 @@ enum MonthlyStatsContext: string
         return match ($this) {
             MonthlyStatsContext::MOVING_TIME => 'time',
             default => $this->value,
+        };
+    }
+
+    public function trans(TranslatorInterface $translator, ?string $locale = null): string
+    {
+        return match ($this) {
+            self::MOVING_TIME => $translator->trans('Moving time', locale: $locale),
+            self::DISTANCE => $translator->trans('Distance', locale: $locale),
+            self::ELEVATION => $translator->trans('Elevation', locale: $locale),
         };
     }
 }
