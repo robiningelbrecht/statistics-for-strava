6fd1cbc8c40608795f5d8a541dd1e1511425ebda
Robin Ingelbrecht <r.ingelbrecht@codeshare.co>
2025-08-29 09:20:47 +0200

    ISSUE #1039: Allow to hide photos per sportType

--- src/BuildApp/BuildPhotosHtml/BuildPhotosHtmlCommandHandler.php
+++ src/BuildApp/BuildPhotosHtml/BuildPhotosHtmlCommandHandler.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\BuildApp\BuildPhotosHtml;
 
 use App\Domain\Activity\Image\ImageRepository;
+use App\Domain\Activity\SportType\SportType;
 use App\Domain\Activity\SportType\SportTypeRepository;
+use App\Domain\Activity\SportType\SportTypes;
 use App\Infrastructure\CQRS\Command\Command;
 use App\Infrastructure\CQRS\Command\CommandHandler;
 use League\Flysystem\FilesystemOperator;
@@ -16,6 +18,7 @@ final readonly class BuildPhotosHtmlCommandHandler implements CommandHandler
     public function __construct(
         private ImageRepository $imageRepository,
         private SportTypeRepository $sportTypeRepository,
+        private HidePhotosForSportTypes $hidePhotosForSportTypes,
         private Environment $twig,
         private FilesystemOperator $buildStorage,
     ) {
@@ -26,13 +29,17 @@ final readonly class BuildPhotosHtmlCommandHandler implements CommandHandler
         assert($command instanceof BuildPhotosHtml);
 
         $importedSportTypes = $this->sportTypeRepository->findAll();
-        $images = $this->imageRepository->findAll();
+        $sportTypesToRenderPhotosFor = SportTypes::fromArray(array_filter(
+            $importedSportTypes->toArray(),
+            fn (SportType $sportType) => !$this->hidePhotosForSportTypes->has($sportType),
+        ));
+        $images = $this->imageRepository->findBySportTypes($sportTypesToRenderPhotosFor);
 
         $this->buildStorage->write(
             'photos.html',
             $this->twig->load('html/photos.html.twig')->render([
                 'images' => $images,
-                'sportTypes' => $importedSportTypes,
+                'sportTypes' => $sportTypesToRenderPhotosFor,
             ]),
         );
     }
