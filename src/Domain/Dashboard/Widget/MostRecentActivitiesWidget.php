b2aa7176b12462864ecd247f5eb745fa2afef8e3
GrabBug <156555693+GrabBug@users.noreply.github.com>
2025-09-21 14:37:04 -0400

    Update MostRecentActivitiesWidget.php
    Replaced hardcoded 5 with numberOfActivitiesToDisplay which still defaults to 5.

--- src/Domain/Dashboard/Widget/MostRecentActivitiesWidget.php
+++ src/Domain/Dashboard/Widget/MostRecentActivitiesWidget.php
@@ -18,19 +18,34 @@ final readonly class MostRecentActivitiesWidget implements Widget
 
     public function getDefaultConfiguration(): WidgetConfiguration
     {
-        return WidgetConfiguration::empty();
+        return WidgetConfiguration::empty()
+            ->add('numberOfActivitiesToDisplay', 5);
     }
 
     public function guardValidConfiguration(array $config): void
     {
+        if (!array_key_exists('numberOfActivitiesToDisplay', $config)) {
+            throw new InvalidDashboardLayout('Configuration item "numberOfActivitiesToDisplay" is required for MostRecentActivitiesWidget.');
+        }
+
+        if (!is_int($config['numberOfActivitiesToDisplay'])) {
+            throw new InvalidDashboardLayout('Configuration item "numberOfActivitiesToDisplay" must be an integer.');
+        }
+
+        if ($config['numberOfActivitiesToDisplay'] < 1) {
+            throw new InvalidDashboardLayout('Configuration item "numberOfActivitiesToDisplay" must be set to a value of 1 or greater.');
+        }
     }
 
     public function render(SerializableDateTime $now, WidgetConfiguration $configuration): string
     {
         $allActivities = $this->activitiesEnricher->getEnrichedActivities();
 
+        /** @var int $numberOfActivitiesToDisplay */
+        $numberOfActivitiesToDisplay = $configuration->getConfigItem('numberOfActivitiesToDisplay');
+
         return $this->twig->load('html/dashboard/widget/widget--most-recent-activities.html.twig')->render([
-            'mostRecentActivities' => $allActivities->slice(0, 5),
+            'mostRecentActivities' => $allActivities->slice(0, $numberOfActivitiesToDisplay),
         ]);
     }
 }
