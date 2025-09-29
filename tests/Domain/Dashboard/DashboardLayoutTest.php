7cb9dfd562518b55f150ee65043d35fb0ca071d6
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-09-29 12:47:46 +0200

    ISSUE #1122: Add enableLastXYearsByDefault config option to yearlyDistances widget

--- tests/Domain/Dashboard/DashboardLayoutTest.php
+++ tests/Domain/Dashboard/DashboardLayoutTest.php
@@ -31,7 +31,7 @@ class DashboardLayoutTest extends TestCase
                 ['widget' => 'dayTimeStats', 'width' => 50, 'enabled' => true],
                 ['widget' => 'distanceBreakdown', 'width' => 100, 'enabled' => true],
                 ['widget' => 'bestEfforts', 'width' => 100, 'enabled' => true],
-                ['widget' => 'yearlyDistances', 'width' => 100, 'enabled' => true],
+                ['widget' => 'yearlyDistances', 'width' => 100, 'enabled' => true, 'config' => ['enableLastXYearsByDefault' => 10]],
                 ['widget' => 'gearStats', 'width' => 50, 'enabled' => true, 'config' => ['includeRetiredGear' => true]],
                 ['widget' => 'eddington', 'width' => 50, 'enabled' => true],
                 ['widget' => 'challengeConsistency', 'width' => 50, 'enabled' => true],
