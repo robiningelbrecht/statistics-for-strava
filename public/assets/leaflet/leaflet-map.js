ab9eebb3d85094e61fcfaf07686b804c619146bd
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-09-29 10:10:31 +0200

    ISSUE #1097: Show position on leaflet map when hovering charts

--- public/assets/leaflet/leaflet-map.js
+++ public/assets/leaflet/leaflet-map.js
@@ -40,18 +40,22 @@ export default function LeafletMap($mapNode, data) {
         // First add a marker to the map.
         const marker = L.circleMarker([0, 0], {
             radius: 6,
-            color: 'red',
-            fillColor: 'red',
+            color: '#303030',
+            fillColor: '#F26722',
             fillOpacity: 0,
             opacity: 0
         }).addTo(map);
 
         const chart = echarts.getInstanceByDom($correspondingEChartNode);
+        const initialZoom = map.getZoom();
 
         chart.on('updateAxisPointer', function (event) {
             if (event.dataIndex && event.dataIndex in coordinateMap) {
                 marker.setLatLng(coordinateMap[event.dataIndex]);
                 marker.setStyle({opacity: 1, fillOpacity: 1});
+                if(map.getZoom() > initialZoom) {
+                    map.panTo(marker.getLatLng());
+                }
             } else {
                 marker.setStyle({opacity: 0, fillOpacity: 0});
             }
