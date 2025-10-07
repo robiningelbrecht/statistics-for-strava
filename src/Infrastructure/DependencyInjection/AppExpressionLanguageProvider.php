bdc231b60ac3827479f15d1264b9cc87dbf9ccb8
Robin Ingelbrecht <ingebrechtrobin@gmail.com>
2025-10-07 16:08:15 +0200

    ISSUE #1160: Add rector to codebase

--- src/Infrastructure/DependencyInjection/AppExpressionLanguageProvider.php
+++ src/Infrastructure/DependencyInjection/AppExpressionLanguageProvider.php
@@ -15,7 +15,11 @@ final readonly class AppExpressionLanguageProvider implements ExpressionFunction
     public function getFunctions(): array
     {
         return [
-            new ExpressionFunction('app_config', fn (string $appConfigKey, mixed $defaultValue = null): string => sprintf('$container->get("app.config")->get(%s, %s)', $appConfigKey, $defaultValue), fn (array $variables, string $appConfigKey, mixed $defaultValue = null): string => $variables['container']->get('app.config')->get($appConfigKey, $defaultValue)),
+            new ExpressionFunction(
+                'app_config',
+                fn (string $appConfigKey, mixed $defaultValue = null): string => sprintf('$container->get("app.config")->get(%s, %s)', $appConfigKey, $defaultValue),
+                fn (array $variables, string $appConfigKey, mixed $defaultValue = null): string => $variables['container']->get('app.config')->get($appConfigKey, $defaultValue)
+            ),
         ];
     }
 }
