30ec460fe0c893789a19d104ff85946943d5153f
Robin Ingelbrecht <ingebrechtrobin@gmail.com>
2025-10-10 15:17:02 +0200

    ISSUE #1178: Rework renderActivityTitleLink

--- tests/Infrastructure/Twig/UrlTwigExtensionTest.php
+++ tests/Infrastructure/Twig/UrlTwigExtensionTest.php
@@ -3,28 +3,55 @@
 namespace App\Tests\Infrastructure\Twig;
 
 use App\BuildApp\AppUrl;
+use App\Infrastructure\Twig\StringTwigExtension;
+use App\Infrastructure\Twig\SvgsTwigExtension;
 use App\Infrastructure\Twig\UrlTwigExtension;
-use PHPUnit\Framework\TestCase;
+use App\Tests\ContainerTestCase;
 
-class UrlTwigExtensionTest extends TestCase
+class UrlTwigExtensionTest extends ContainerTestCase
 {
+    private StringTwigExtension $stringTwigExtension;
+    private SvgsTwigExtension $svgsTwigExtension;
+
     public function testToAbsoluteUrl(): void
     {
         $this->assertEquals(
             '/test/path',
-            new UrlTwigExtension(AppUrl::fromString('http://localhost:8081'))->toRelativeUrl('test/path')
+            new UrlTwigExtension(
+                appUrl: AppUrl::fromString('http://localhost:8081'),
+                stringTwigExtension: $this->stringTwigExtension,
+                svgsTwigExtension: $this->svgsTwigExtension,
+            )->toRelativeUrl('test/path')
         );
         $this->assertEquals(
             '/test/path',
-            new UrlTwigExtension(AppUrl::fromString('http://localhost:8081'))->toRelativeUrl('/test/path')
+            new UrlTwigExtension(
+                appUrl: AppUrl::fromString('http://localhost:8081'),
+                stringTwigExtension: $this->stringTwigExtension,
+                svgsTwigExtension: $this->svgsTwigExtension,
+            )->toRelativeUrl('/test/path')
         );
         $this->assertEquals(
             '/base/test/path',
-            new UrlTwigExtension(AppUrl::fromString('http://localhost:8081/base/'))->toRelativeUrl('test/path')
+            new UrlTwigExtension(
+                appUrl: AppUrl::fromString('http://localhost:8081/base/'),
+                stringTwigExtension: $this->stringTwigExtension,
+                svgsTwigExtension: $this->svgsTwigExtension,
+            )->toRelativeUrl('test/path')
         );
         $this->assertEquals(
             '/base/test/path',
-            new UrlTwigExtension(AppUrl::fromString('http://localhost:8081/base/'))->toRelativeUrl('/test/path')
+            new UrlTwigExtension(
+                appUrl: AppUrl::fromString('http://localhost:8081/base/'),
+                stringTwigExtension: $this->stringTwigExtension,
+                svgsTwigExtension: $this->svgsTwigExtension,
+            )->toRelativeUrl('/test/path')
         );
+    }
+
+    protected function setUp(): void
+    {
+        $this->stringTwigExtension = new StringTwigExtension();
+        $this->svgsTwigExtension = new SvgsTwigExtension();
     }
 }
