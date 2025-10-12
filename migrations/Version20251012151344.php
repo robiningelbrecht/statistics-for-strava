221478da4965fd74befe2b68d10cc2beccbf7f1c
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-10-12 17:24:57 +0200

    ISSUE #1187: Add power and cadance to combined activity charts for running

---
+++ migrations/Version20251012151344.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use App\Domain\Activity\ActivityType;
+use Doctrine\DBAL\ArrayParameterType;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251012151344 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $activityIds = $this->connection->fetchFirstColumn(
+            <<<'SQL'
+                SELECT activityId FROM Activity WHERE activityType = :activityType
+            SQL,
+            [
+                'activityType' => ActivityType::RUN->value,
+            ]
+        );
+
+        if(count($activityIds) === 0) {
+            return;
+        }
+
+        $this->addSql(<<<'SQL'
+            DELETE FROM CombinedActivityStream
+            WHERE activityId IN(:activityIds)
+        SQL,
+            [
+                'activityIds' => $activityIds,
+            ],
+            [
+                'activityIds' => ArrayParameterType::STRING,
+            ],
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    }
+}
