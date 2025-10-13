3810b8d758abf4fd968636b92963e83ef9b63fb9
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-10-13 14:12:38 +0200

    Bumped APP version to v3.7.0

--- src/BuildApp/AppVersion.php
+++ src/BuildApp/AppVersion.php
@@ -7,8 +7,8 @@ namespace App\BuildApp;
 final readonly class AppVersion
 {
     private const int MAJOR = 3;
-    private const int MINOR = 6;
-    private const int PATCH = 3;
+    private const int MINOR = 7;
+    private const int PATCH = 0;
 
     public static function getSemanticVersion(): string
     {
