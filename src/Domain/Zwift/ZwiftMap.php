dee69221cda54d5fa0f9e62aaf3dcbbca7ab04d3
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-11-10 19:53:30 +0100

    Better New York Zwift map

--- src/Domain/Zwift/ZwiftMap.php
+++ src/Domain/Zwift/ZwiftMap.php
@@ -98,7 +98,7 @@ final readonly class ZwiftMap implements LeafletMap
             ],
             self::NEW_YORK => [
                 Coordinate::createFromLatAndLng(Latitude::fromString('40.81725'), Longitude::fromString('-74.0227')),
-                Coordinate::createFromLatAndLng(Latitude::fromString('40.58805'), Longitude::fromString('-73.9222')),
+                Coordinate::createFromLatAndLng(Latitude::fromString('40.6385'), Longitude::fromString('-73.9222')),
             ],
             self::PARIS => [
                 Coordinate::createFromLatAndLng(Latitude::fromString('48.9058'), Longitude::fromString('2.2561')),
