505c56fd78bc4edce1664d7ebc65280439ff4dd5
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-11-11 18:48:37 +0100

    ISSUE #1371: Do not crash import when nominatim fails us

--- src/Domain/Integration/Geocoding/Nominatim/LiveNominatim.php
+++ src/Domain/Integration/Geocoding/Nominatim/LiveNominatim.php
@@ -8,6 +8,8 @@ use App\Infrastructure\Serialization\Json;
 use App\Infrastructure\Time\Sleep;
 use App\Infrastructure\ValueObject\Geography\Coordinate;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\RequestOptions;
 
 final readonly class LiveNominatim implements Nominatim
@@ -20,23 +22,27 @@ final readonly class LiveNominatim implements Nominatim
 
     public function reverseGeocode(Coordinate $coordinate): Location
     {
-        $response = $this->client->request(
-            'GET',
-            'https://nominatim.openstreetmap.org/reverse',
-            [
-                RequestOptions::HEADERS => [
-                    'User-Agent' => 'Statistics for Strava App',
-                ],
-                RequestOptions::QUERY => [
-                    'lat' => $coordinate->getLatitude()->toFloat(),
-                    'lon' => $coordinate->getLongitude()->toFloat(),
-                    'format' => 'json',
-                ],
-            ]
-        );
+        try {
+            $response = $this->client->request(
+                'GET',
+                'https://nominatim.openstreetmap.org/reverse',
+                [
+                    RequestOptions::HEADERS => [
+                        'User-Agent' => 'Statistics for Strava App',
+                    ],
+                    RequestOptions::QUERY => [
+                        'lat' => $coordinate->getLatitude()->toFloat(),
+                        'lon' => $coordinate->getLongitude()->toFloat(),
+                        'format' => 'json',
+                    ],
+                ]
+            );
 
-        $response = Json::decode($response->getBody()->getContents());
-        $this->sleep->sweetDreams(1);
+            $response = Json::decode($response->getBody()->getContents());
+            $this->sleep->sweetDreams(1);
+        } catch (ConnectException|RequestException) {
+            throw new CouldNotReverseGeocodeAddress();
+        }
 
         return Location::create($response['address'] ?? throw new CouldNotReverseGeocodeAddress());
     }
