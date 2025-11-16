106dbf1c75a39fa87a2ad91de4a89ca60c2cf4c0
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-11-16 20:44:25 +0100

    ISSUE #1407: Distribution charts are not calculated for new activities

--- migrations/Version20251116105419.php
+++ migrations/Version20251116105419.php
@@ -37,16 +37,5 @@ final class Version20251116105419 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TEMPORARY TABLE __temp__ActivityStream AS SELECT activityId, streamType, createdOn, data, computedFieldsState, normalizedPower, valueDistribution, bestAverages FROM ActivityStream');
-        $this->addSql('DROP TABLE ActivityStream');
-        $this->addSql('CREATE TABLE ActivityStream (activityId VARCHAR(255) NOT NULL, streamType VARCHAR(255) NOT NULL, createdOn DATETIME NOT NULL --(DC2Type:datetime_immutable)
-        , data CLOB NOT NULL --(DC2Type:json)
-        , computedFieldsState CLOB DEFAULT NULL, normalizedPower INTEGER DEFAULT NULL, valueDistribution CLOB DEFAULT NULL --(DC2Type:json)
-        , bestAverages CLOB DEFAULT NULL --(DC2Type:json)
-        , PRIMARY KEY(activityId, streamType))');
-        $this->addSql('INSERT INTO ActivityStream (activityId, streamType, createdOn, data, computedFieldsState, normalizedPower, valueDistribution, bestAverages) SELECT activityId, streamType, createdOn, data, computedFieldsState, normalizedPower, valueDistribution, bestAverages FROM __temp__ActivityStream');
-        $this->addSql('DROP TABLE __temp__ActivityStream');
-        $this->addSql('CREATE INDEX ActivityStream_activityIndex ON ActivityStream (activityId)');
-        $this->addSql('CREATE INDEX ActivityStream_streamTypeIndex ON ActivityStream (streamType)');
     }
 }
