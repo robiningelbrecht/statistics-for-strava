fa81269b0d317313aa739ad16488e358bd8cacf9
Robin Ingelbrecht <ingebrechtrobin@gmail.com>
2025-11-21 16:38:02 +0100

    ISSUE #1438: Skip OrdSingleByteRector rule for noz

--- rector.php
+++ rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Php85\Rector\FuncCall\OrdSingleByteRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -11,4 +12,5 @@ return RectorConfig::configure()
     ->withPhpSets(php85: true)
     ->withTypeCoverageLevel(10)
     ->withDeadCodeLevel(10)
-    ->withCodeQualityLevel(10);
+    ->withCodeQualityLevel(10)
+    ->withSkip([OrdSingleByteRector::class]);
