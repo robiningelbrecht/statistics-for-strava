69fd221c451f4aa98cf8d70c1b1921bf19826f7d
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-11-28 13:10:37 +0100

    ISSUE #1421: Add running to peak power outputt and FTP history

--- src/Domain/Ftp/FtpHistory.php
+++ src/Domain/Ftp/FtpHistory.php
@@ -4,52 +4,80 @@ declare(strict_types=1);
 
 namespace App\Domain\Ftp;
 
+use App\Domain\Activity\ActivityType;
 use App\Domain\Integration\AI\SupportsAITooling;
 use App\Infrastructure\Exception\EntityNotFound;
 use App\Infrastructure\ValueObject\Time\SerializableDateTime;
 
 final class FtpHistory implements SupportsAITooling
 {
-    /** @var Ftp[] */
+    private const string CYCLING_KEY = 'cycling';
+    private const string RUNNING_KEY = 'running';
+
+    /** @var array<string, Ftp[]> */
     private array $ftps;
 
     /**
-     * @param array<string, int> $ftps
+     * @param array<string, array<string, int>> $ftps
      */
     private function __construct(
         array $ftps,
     ) {
-        $this->ftps = [];
+        $this->ftps[self::CYCLING_KEY] = $this->mapFtpHistory($ftps[self::CYCLING_KEY] ?? [], self::CYCLING_KEY);
+        $this->ftps[self::RUNNING_KEY] = $this->mapFtpHistory($ftps[self::RUNNING_KEY] ?? [], self::RUNNING_KEY);
 
-        foreach ($ftps as $setOn => $ftpValue) {
+        krsort($this->ftps[self::CYCLING_KEY]);
+        krsort($this->ftps[self::RUNNING_KEY]);
+    }
+
+    /**
+     * @param array<string, int> $entries
+     *
+     * @return array<int, Ftp>
+     */
+    private function mapFtpHistory(array $entries, string $type): array
+    {
+        $result = [];
+
+        foreach ($entries as $setOn => $ftpValue) {
             try {
-                $setOnDate = SerializableDateTime::fromString($setOn);
-                $this->ftps[$setOnDate->getTimestamp()] = Ftp::fromState(
-                    setOn: $setOnDate,
+                $date = SerializableDateTime::fromString($setOn);
+                $result[$date->getTimestamp()] = Ftp::fromState(
+                    setOn: $date,
                     ftp: FtpValue::fromInt($ftpValue)
                 );
             } catch (\DateMalformedStringException) {
-                throw new \InvalidArgumentException(sprintf('Invalid date "%s" set for athlete ftpHistory in config.yaml file', $setOn));
+                throw new \InvalidArgumentException(sprintf('Invalid date "%s" set for athlete %s ftpHistory in config.yaml file', $setOn, $type));
             }
         }
 
-        krsort($this->ftps);
+        return $result;
     }
 
-    public function findAll(): Ftps
+    public function findAll(ActivityType $activityType): Ftps
     {
-        $ftps = $this->ftps;
+        $ftps = match ($activityType) {
+            ActivityType::RIDE => $this->ftps[self::CYCLING_KEY],
+            ActivityType::RUN => $this->ftps[self::RUNNING_KEY],
+            default => throw new \RuntimeException(sprintf('ActivityType "%s" does not support FTP', $activityType->value)),
+        };
+
         // We want to sort by date in ascending order
         ksort($ftps);
 
         return Ftps::fromArray($ftps);
     }
 
-    public function find(SerializableDateTime $on): Ftp
+    public function find(ActivityType $activityType, SerializableDateTime $on): Ftp
     {
         $on = SerializableDateTime::fromString($on->format('Y-m-d'));
-        /** @var Ftp $ftp */
-        foreach ($this->ftps as $ftp) {
+        $ftps = match ($activityType) {
+            ActivityType::RIDE => $this->ftps[self::CYCLING_KEY],
+            ActivityType::RUN => $this->ftps[self::RUNNING_KEY],
+            default => throw new \RuntimeException(sprintf('ActivityType "%s" does not support FTP', $activityType->value)),
+        };
+
+        foreach ($ftps as $ftp) {
             if ($on->isAfterOrOn($ftp->getSetOn())) {
                 return $ftp;
             }
@@ -64,7 +92,7 @@ final class FtpHistory implements SupportsAITooling
     public function exportForAITooling(): array
     {
         $history = [];
-        foreach ($this->findAll() as $ftp) {
+        foreach ($this->findAll(ActivityType::RIDE) as $ftp) {
             $history[] = [
                 'setOn' => $ftp->getSetOn()->format('Y-m-d'),
                 'ftpValue' => $ftp->getFtp()->getValue(),
@@ -75,10 +103,17 @@ final class FtpHistory implements SupportsAITooling
     }
 
     /**
-     * @param array<string, int> $values
+     * @param array<string, mixed> $values
      */
     public static function fromArray(array $values): self
     {
+        if (empty($values[self::CYCLING_KEY]) && empty($values[self::RUNNING_KEY])) {
+            // This is still an old FTP history when we didn't
+            // differentiate between cycling and running yet. Make sure it's BC.
+            $values[self::CYCLING_KEY] = $values;
+        }
+
+        /* @var array<string, array<string, int>> $values */
         return new self($values);
     }
 }
