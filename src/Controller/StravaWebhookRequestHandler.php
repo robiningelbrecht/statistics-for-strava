6a45b511fe939ad359190112c237d644ee3b2c7b
robiningelbrecht <ingebrechtrobin@gmail.com>
2025-12-03 09:26:00 +0100

    ISSUE #1373: Upgrade to Symfony 8

--- src/Controller/StravaWebhookRequestHandler.php
+++ src/Controller/StravaWebhookRequestHandler.php
@@ -13,7 +13,7 @@ use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[WithMonologChannel('webhooks')]
 final readonly class StravaWebhookRequestHandler
