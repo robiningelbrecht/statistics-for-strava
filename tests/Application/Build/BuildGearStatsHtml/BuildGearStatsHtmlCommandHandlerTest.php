d10d4c88e612ee73c3e7b2c7a7b56534df2eba51
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-12-13 19:13:17 +0100

    ISSUE #1529: Show warning when not all gear has been imported yet

--- tests/Application/Build/BuildGearStatsHtml/BuildGearStatsHtmlCommandHandlerTest.php
+++ tests/Application/Build/BuildGearStatsHtml/BuildGearStatsHtmlCommandHandlerTest.php
@@ -3,14 +3,27 @@
 namespace App\Tests\Application\Build\BuildGearStatsHtml;
 
 use App\Application\Build\BuildGearStatsHtml\BuildGearStatsHtml;
+use App\Domain\Activity\ActivityId;
+use App\Domain\Activity\ActivityWithRawData;
+use App\Domain\Activity\ActivityWithRawDataRepository;
+use App\Domain\Gear\GearId;
 use App\Infrastructure\ValueObject\Time\SerializableDateTime;
 use App\Tests\Application\BuildAppFilesTestCase;
+use App\Tests\Domain\Activity\ActivityBuilder;
 
 class BuildGearStatsHtmlCommandHandlerTest extends BuildAppFilesTestCase
 {
     public function testHandle(): void
     {
         $this->provideFullTestSet();
+
+        $this->getContainer()->get(ActivityWithRawDataRepository::class)->add(ActivityWithRawData::fromState(
+            activity: ActivityBuilder::fromDefaults()
+                ->withActivityId(ActivityId::fromUnprefixed('testy'))
+                ->withGearId(GearId::fromUnprefixed('testy'))
+                ->build(),
+            rawData: []
+        ));
 
         $this->commandBus->dispatch(new BuildGearStatsHtml(SerializableDateTime::fromString('2023-10-17 16:15:04')));
         $this->assertFileSystemWrites($this->getContainer()->get('build.storage'));
