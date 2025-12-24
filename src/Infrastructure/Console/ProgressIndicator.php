cb2fc8792eec510f5edc84a89124895eaa357459
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2025-12-24 15:03:54 +0100

    ISSUE #1584: Add progress bar to CLI output

--- src/Infrastructure/Console/ProgressIndicator.php
+++ src/Infrastructure/Console/ProgressIndicator.php
@@ -4,16 +4,21 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Console;
 
+use App\Infrastructure\ValueObject\String\PlatformEnvironment;
+use Symfony\Component\Console\Helper\Helper;
 use Symfony\Component\Console\Helper\ProgressIndicator as SymfonyProgressIndicator;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ProgressIndicator
+readonly class ProgressIndicator
 {
-    private readonly SymfonyProgressIndicator $symfonyProgressIndicator;
+    private SymfonyProgressIndicator $symfonyProgressIndicator;
 
     public function __construct(OutputInterface $output)
     {
+        $startTime = time();
+
         SymfonyProgressIndicator::setPlaceholderFormatterDefinition('indicator', static fn (): string => '');
+        SymfonyProgressIndicator::setPlaceholderFormatterDefinition('elapsed', fn () => PlatformEnvironment::fromServer()->isTest() ? '3 s' : Helper::formatTime(time() - $startTime, 2));
 
         $this->symfonyProgressIndicator = new SymfonyProgressIndicator(
             output: $output,
