c993262496bb99950c70961993e925bfd288150a
robiningelbrecht <ingebrechtrobin@gmail.com>
2026-01-04 10:20:04 +0100

    ISSUE #1608: Segment effort history chart

--- src/Domain/Dashboard/Widget/YearlyStats/YearlyStatisticsChart.php
+++ src/Domain/Dashboard/Widget/YearlyStats/YearlyStatisticsChart.php
@@ -15,7 +15,7 @@ use App\Infrastructure\ValueObject\Time\SerializableDateTime;
 use App\Infrastructure\ValueObject\Time\Years;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
-final readonly class YearlyDistanceChart
+final readonly class YearlyStatisticsChart
 {
     private function __construct(
         private FindYearlyStatsPerDayResponse $yearStats,
