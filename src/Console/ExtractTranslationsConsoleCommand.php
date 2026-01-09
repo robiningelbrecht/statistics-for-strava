16f5bb94e14013110003053b80bfab83da42f9c2
robiningelbrecht <ingebrechtrobin@gmail.com>
2026-01-09 18:42:51 +0100

    ISSUE #1631: Improve ExtractTranslationsConsoleCommand

--- src/Console/ExtractTranslationsConsoleCommand.php
+++ src/Console/ExtractTranslationsConsoleCommand.php
@@ -5,15 +5,32 @@ declare(strict_types=1);
 namespace App\Console;
 
 use App\Infrastructure\Localisation\Locale;
+use App\Infrastructure\ValueObject\String\KernelProjectDir;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
+use Symfony\Component\Translation\Extractor\ExtractorInterface;
+use Symfony\Component\Translation\MessageCatalogue;
+use Symfony\Component\Yaml\Yaml;
 
 #[AsCommand(name: 'app:translations:extract', description: 'Extract translations for all locales')]
 class ExtractTranslationsConsoleCommand extends Command
 {
+    public function __construct(
+        private readonly ExtractorInterface $extractor,
+        private readonly KernelProjectDir $kernelProjectDir
+    ){
+        parent::__construct();
+    }
+    protected function configure(): void
+    {
+        $this->addOption('removeObsoleteTranslatables', null, InputOption::VALUE_NONE);
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         foreach (Locale::cases() as $locale) {
@@ -29,11 +46,25 @@ class ExtractTranslationsConsoleCommand extends Command
                     '--domain=messages',
                     $locale->value,
                 ],
-                timeout: null
+                timeout: null,
             );
 
             $process->run();
             $output->writeln(sprintf('<info>Extracted translations for "%s"</info>', $locale->value));
+        }
+
+        if($input->getOption('removeObsoleteTranslatables')){
+            $messages = new MessageCatalogue(Locale::en_US->value);
+
+            $this->extractor->extract($this->kernelProjectDir.'/templates', $messages);
+            $this->extractor->extract($this->kernelProjectDir.'/src', $messages);
+            $translatables = $messages->all()['messages'];
+
+            $translatableKeys = array_keys($translatables ?? []);
+
+            $translationFilePath = sprintf('%s/translations/messages%s.%s.yaml', $this->kernelProjectDir, MessageCatalogue::INTL_DOMAIN_SUFFIX, Locale::en_US->value);
+            $parsedTranslations = Yaml::parse(file_get_contents($translationFilePath));
+            $translationKeysToRemove = array_diff(array_keys($parsedTranslations), $translatableKeys);
         }
 
         return Command::SUCCESS;
