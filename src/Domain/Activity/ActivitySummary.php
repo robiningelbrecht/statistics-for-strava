0c7c44deeb082d64e009ff9585d1003670d1cf62
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-01-17 20:13:06 +0100

    ISSUE #1659: Introduce EnrichedActivities

--- src/Domain/Activity/ActivitySummary.php
+++ src/Domain/Activity/ActivitySummary.php
@@ -8,7 +8,6 @@ use App\Infrastructure\ValueObject\Time\SerializableDateTime;
 final readonly class ActivitySummary
 {
     private function __construct(
-        private ActivityId $activityId,
         private string $name,
         private SerializableDateTime $startDateTime,
         private SportType $sportType,
@@ -16,22 +15,15 @@ final readonly class ActivitySummary
     }
 
     public static function create(
-        ActivityId $activityId,
         string $name,
         SerializableDateTime $startDateTime,
         SportType $sportType,
     ): self {
         return new self(
-            activityId: $activityId,
             name: $name,
             startDateTime: $startDateTime,
             sportType: $sportType,
         );
-    }
-
-    public function getActivityId(): ActivityId
-    {
-        return $this->activityId;
     }
 
     public function getName(): string
