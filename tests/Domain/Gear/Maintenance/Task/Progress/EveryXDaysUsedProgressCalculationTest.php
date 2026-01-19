43a8e043a56b4129f25b366e69191b610f2d61ec
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-01-19 10:23:46 +0100

    ISSUE #1669: Gear maintenance - every x days used

---
+++ tests/Domain/Gear/Maintenance/Task/Progress/EveryXDaysUsedProgressCalculationTest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Tests\Domain\Gear\Maintenance\Task\Progress;
+
+use App\Domain\Activity\ActivityId;
+use App\Domain\Activity\ActivityWithRawData;
+use App\Domain\Activity\ActivityWithRawDataRepository;
+use App\Domain\Gear\GearId;
+use App\Domain\Gear\GearIds;
+use App\Domain\Gear\Maintenance\GearMaintenanceConfig;
+use App\Domain\Gear\Maintenance\Task\IntervalUnit;
+use App\Domain\Gear\Maintenance\Task\Progress\EveryXDaysUsedProgressCalculation;
+use App\Domain\Gear\Maintenance\Task\Progress\MaintenanceTaskProgress;
+use App\Domain\Gear\Maintenance\Task\Progress\ProgressCalculationContext;
+use App\Infrastructure\ValueObject\Time\SerializableDateTime;
+use App\Tests\ContainerTestCase;
+use App\Tests\Domain\Activity\ActivityBuilder;
+use Doctrine\DBAL\Connection;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class EveryXDaysUsedProgressCalculationTest extends ContainerTestCase
+{
+    private EveryXDaysUsedProgressCalculation $calculation;
+
+    public function testCalculate(): void
+    {
+        $this->getContainer()->get(ActivityWithRawDataRepository::class)->add(ActivityWithRawData::fromState(
+            ActivityBuilder::fromDefaults()
+                ->withActivityId(ActivityId::fromUnprefixed('last-tagged'))
+                ->withStartDateTime(SerializableDateTime::fromString('2025-01-01 00:00:00'))
+                ->build(),
+            []
+        ));
+
+        $this->getContainer()->get(ActivityWithRawDataRepository::class)->add(ActivityWithRawData::fromState(
+            ActivityBuilder::fromDefaults()
+                ->withActivityId(ActivityId::fromUnprefixed('include'))
+                ->withGearId(GearId::fromUnprefixed('test'))
+                ->withStartDateTime(SerializableDateTime::fromString('2025-01-01 01:00:00'))
+                ->withMovingTimeInSeconds(3600)
+                ->build(),
+            []
+        ));
+
+        $this->getContainer()->get(ActivityWithRawDataRepository::class)->add(ActivityWithRawData::fromState(
+            ActivityBuilder::fromDefaults()
+                ->withActivityId(ActivityId::fromUnprefixed('include-2'))
+                ->withGearId(GearId::fromUnprefixed('test'))
+                ->withStartDateTime(SerializableDateTime::fromString('2025-01-01 02:00:00'))
+                ->withMovingTimeInSeconds(3600)
+                ->build(),
+            []
+        ));
+
+        $this->getContainer()->get(ActivityWithRawDataRepository::class)->add(ActivityWithRawData::fromState(
+            ActivityBuilder::fromDefaults()
+                ->withActivityId(ActivityId::fromUnprefixed('include-3'))
+                ->withGearId(GearId::fromUnprefixed('test'))
+                ->withStartDateTime(SerializableDateTime::fromString('2025-01-02 02:00:00'))
+                ->withMovingTimeInSeconds(3600)
+                ->build(),
+            []
+        ));
+
+        $this->getContainer()->get(ActivityWithRawDataRepository::class)->add(ActivityWithRawData::fromState(
+            ActivityBuilder::fromDefaults()
+                ->withActivityId(ActivityId::fromUnprefixed('1'))
+                ->withGearId(GearId::random())
+                ->withMovingTimeInSeconds(3600)
+                ->build(),
+            []
+        ));
+
+        $this->getContainer()->get(ActivityWithRawDataRepository::class)->add(ActivityWithRawData::fromState(
+            ActivityBuilder::fromDefaults()
+                ->withActivityId(ActivityId::fromUnprefixed('2'))
+                ->withGearId(GearId::fromUnprefixed('test'))
+                ->withStartDateTime(SerializableDateTime::fromString('2024-01-01 00:00:00'))
+                ->withMovingTimeInSeconds(3600)
+                ->build(),
+            []
+        ));
+
+        $this->assertEquals(
+            MaintenanceTaskProgress::from(
+                20,
+                '2 days',
+            ),
+            $this->calculation->calculate(
+                ProgressCalculationContext::from(
+                    gearIds: GearIds::fromArray([GearId::fromUnprefixed('test')]),
+                    lastTaggedOnActivityId: ActivityId::fromUnprefixed('last-tagged'),
+                    lastTaggedOn: SerializableDateTime::fromString('01-01-2025'),
+                    intervalUnit: IntervalUnit::EVERY_X_HOURS_USED,
+                    intervalValue: 10,
+                )
+            )
+        );
+    }
+
+    #[\Override]
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->calculation = new EveryXDaysUsedProgressCalculation(
+            $this->getContainer()->get(Connection::class),
+            $this->getContainer()->get(GearMaintenanceConfig::class),
+            $this->getContainer()->get(TranslatorInterface::class),
+        );
+    }
+}
