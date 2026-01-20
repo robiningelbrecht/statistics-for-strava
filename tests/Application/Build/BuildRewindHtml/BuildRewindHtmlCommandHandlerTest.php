2c1a732e53ccad83cf6ec1ef7dc910325a54ad6d
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-01-20 21:01:02 +0100

    ISSUE #1677: Webhook fails due to Invalid RESTING_HEART_RATE_FORMULA

--- tests/Application/Build/BuildRewindHtml/BuildRewindHtmlCommandHandlerTest.php
+++ tests/Application/Build/BuildRewindHtml/BuildRewindHtmlCommandHandlerTest.php
@@ -7,6 +7,11 @@ use App\Domain\Activity\ActivityId;
 use App\Domain\Activity\ActivityWithRawData;
 use App\Domain\Activity\ActivityWithRawDataRepository;
 use App\Domain\Activity\SportType\SportType;
+use App\Infrastructure\KeyValue\Key;
+use App\Infrastructure\KeyValue\KeyValue;
+use App\Infrastructure\KeyValue\KeyValueStore;
+use App\Infrastructure\KeyValue\Value;
+use App\Infrastructure\Serialization\Json;
 use App\Infrastructure\ValueObject\Time\SerializableDateTime;
 use App\Tests\Application\BuildAppFilesTestCase;
 use App\Tests\Domain\Activity\ActivityBuilder;
@@ -32,6 +37,13 @@ class BuildRewindHtmlCommandHandlerTest extends BuildAppFilesTestCase
 
     public function testHandleWhenNoRewindsToCompare(): void
     {
+        /** @var KeyValueStore $keyValueStore */
+        $keyValueStore = $this->getContainer()->get(KeyValueStore::class);
+        $keyValueStore->save(KeyValue::fromState(
+            Key::THEME,
+            Value::fromString(Json::encode([])),
+        ));
+
         $this->getContainer()->get(ActivityWithRawDataRepository::class)->add(ActivityWithRawData::fromState(
             ActivityBuilder::fromDefaults()
                 ->withActivityId(ActivityId::random())
