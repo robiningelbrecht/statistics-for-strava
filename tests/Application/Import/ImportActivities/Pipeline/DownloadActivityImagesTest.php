6dd2f0222924216c507ce2e5d388da19e1c1e9a6
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-02-02 17:25:57 +0100

    ISSUE #1703: Add proper tests for new TrainingLoadForecast feature

--- tests/Application/Import/ImportActivities/Pipeline/DownloadActivityImagesTest.php
+++ tests/Application/Import/ImportActivities/Pipeline/DownloadActivityImagesTest.php
@@ -9,12 +9,14 @@ use App\Domain\Strava\Strava;
 use App\Infrastructure\ValueObject\Identifier\UuidFactory;
 use App\Tests\ContainerTestCase;
 use App\Tests\Domain\Activity\ActivityBuilder;
+use App\Tests\Domain\Strava\SpyStrava;
 use League\Flysystem\Filesystem;
 use League\Flysystem\InMemory\InMemoryFilesystemAdapter;
 
 class DownloadActivityImagesTest extends ContainerTestCase
 {
     private DownloadActivityImages $downloadActivityImages;
+    private SpyStrava $strava;
 
     public function testProcessWhenImagesShouldNotBeDownloaded(): void
     {
@@ -35,13 +37,35 @@ class DownloadActivityImagesTest extends ContainerTestCase
         );
     }
 
+    public function testProcessWhenClientExceptionIsThrown(): void
+    {
+        $this->strava->setMaxNumberOfCallsBeforeTriggering429(1000);
+        $this->strava->triggerExceptionOnNextCall();
+
+        $context = ActivityImportContext::create(
+            activityId: ActivityId::fromUnprefixed(1),
+            rawStravaData: ['total_photo_count' => 3],
+            isNewActivity: true,
+        )
+            ->withActivity(
+                ActivityBuilder::fromDefaults()
+                    ->withLocalImagePaths('one', 'two', 'three')
+                    ->build()
+            );
+
+        $this->assertEquals(
+            $context,
+            $this->downloadActivityImages->process($context)
+        );
+    }
+
     #[\Override]
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->downloadActivityImages = new DownloadActivityImages(
-            $this->getContainer()->get(Strava::class),
+            $this->strava = $this->getContainer()->get(Strava::class),
             new Filesystem(new InMemoryFilesystemAdapter()),
             $this->getContainer()->get(UuidFactory::class)
         );
