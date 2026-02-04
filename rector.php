ffde6f6849b63ce64f7987f0cc58e56360cc5749
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-02-04 12:45:45 +0100

    ISSUE #1710: Tighten Rector rules

--- rector.php
+++ rector.php
@@ -3,8 +3,17 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Php85\Rector\FuncCall\OrdSingleByteRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(__DIR__.'/rector-src.php');
-    $rectorConfig->import(__DIR__.'/rector-tests.php');
-};
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/src',
+    ])
+    ->withPhpSets(php85: true)
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        earlyReturn: true,
+    )
+    ->withSkip([OrdSingleByteRector::class]);
