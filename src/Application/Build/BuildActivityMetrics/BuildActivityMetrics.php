d619ec003006ca148720505a27bb55eb9d1e5b44
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-02-14 10:05:09 +0100

    ISSUE #1745: USe relativeUrl() for api calls

---
+++ src/Application/Build/BuildActivityMetrics/BuildActivityMetrics.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Build\BuildActivityMetrics;
+
+use App\Infrastructure\CQRS\Command\DomainCommand;
+
+final readonly class BuildActivityMetrics extends DomainCommand
+{
+}
