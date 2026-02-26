1051d4feca4978b450d3cb1a337ea12be1c07f06
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-02-26 18:16:07 +0100

    ISSUE #1828: Add ContentHash

--- migrations/Version20260226161339.php
+++ migrations/Version20260226161339.php
@@ -23,6 +23,7 @@ final class Version20260226161339 extends AbstractMigration
         $this->addSql('CREATE TABLE CacheTagDependency (entityType VARCHAR(255) NOT NULL, entityId VARCHAR(255) NOT NULL, dependsOnTag VARCHAR(255) NOT NULL, PRIMARY KEY (entityType, entityId, dependsOnTag))');
         $this->addSql('CREATE INDEX CacheTagDependency_dependsOnTag ON CacheTagDependency (dependsOnTag)');
         $this->addSql('CREATE TABLE InvalidatedCacheTag (tag VARCHAR(255) NOT NULL, PRIMARY KEY (tag))');
+        $this->addSql('CREATE TABLE ContentHash (entityType VARCHAR(255) NOT NULL, entityId VARCHAR(255) NOT NULL, hash VARCHAR(255) NOT NULL, PRIMARY KEY (entityType, entityId))');
     }
 
     public function down(Schema $schema): void
