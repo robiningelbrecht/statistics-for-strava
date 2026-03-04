3a52832bb0b9a2224dc6844df6fab1f35123ce31
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-03-04 10:55:39 +0100

    ISSUE #1849: Sync PHP internal timezone with system TZ for cron scheduling

--- docker/app/docker-entrypoint.sh
+++ docker/app/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+echo "date.timezone=\"${TZ:-UTC}\"" > "${PHP_INI_DIR}/conf.d/timezone.ini"
+
 if [ -n "$PUID" ] && [ -n "$PGID" ] && [ "$(id -u)" = "0" ]; then
     echo "Setting permissions for PUID=$PUID PGID=$PGID..."
 
