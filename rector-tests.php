a51ed69bef9eac6cbeac9a42fd8646738f4c09b2
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-03-04 10:55:39 +0100

    ISSUE #1849: Sync PHP internal timezone with system TZ for cron scheduling

--- rector-tests.php
+++ rector-tests.php
@@ -3,10 +3,14 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/tests',
     ])
     ->withPhpSets(php85: true)
-    ->withComposerBased(phpunit: true);
+    ->withComposerBased(phpunit: true)
+    ->withConfiguredRule(AddOverrideAttributeToOverriddenMethodsRector::class, [
+        'allow_override_empty_method' => false,
+    ]);
