1416301fff112768bc98398dacab1aba5ec8462c
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-03-10 16:02:34 +0100

    Update vendor packages part 3

--- src/Application/Build/BuildIndexHtml/BuildIndexHtmlCommandHandler.php
+++ src/Application/Build/BuildIndexHtml/BuildIndexHtmlCommandHandler.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace App\Application\Build\BuildIndexHtml;
 
 use App\Application\Router;
+use App\Domain\Athlete\Athlete;
 use App\Infrastructure\CQRS\Command\Command;
 use App\Infrastructure\CQRS\Command\CommandHandler;
 use League\Flysystem\FilesystemOperator;
+use Ramsey\Uuid\Uuid;
 use Twig\Environment;
 
 final readonly class BuildIndexHtmlCommandHandler implements CommandHandler
@@ -23,12 +25,23 @@ final readonly class BuildIndexHtmlCommandHandler implements CommandHandler
     {
         assert($command instanceof BuildIndexHtml);
 
+        $context = $this->indexHtml->getContext($command->getCurrentDateTime());
+        $athlete = $context['athlete'];
+        assert($athlete instanceof Athlete);
+
+        $easterEggPageUrl = Uuid::uuid5(Uuid::NAMESPACE_DNS, $athlete->getAthleteId());
         $this->buildStorage->write(
             'index.html',
             $this->twig->load('html/index.html.twig')->render([
                 'router' => Router::SINGLE_PAGE,
-                ...$this->indexHtml->getContext($command->getCurrentDateTime()),
+                'easterEggPageUrl' => $easterEggPageUrl,
+                ...$context,
             ]),
+        );
+
+        $this->buildStorage->write(
+            sprintf('%s.html', $easterEggPageUrl),
+            $this->twig->load('html/easter-egg.html.twig')->render(),
         );
     }
 }
