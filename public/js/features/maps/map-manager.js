6d9d8e580b92e027d75e017b4685067f645e034c
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-05-22 19:32:43 +0200

    ISSUE #2057: Split up leaflet into separate js file

--- public/js/features/maps/map-manager.js
+++ public/js/features/maps/map-manager.js
@@ -1,8 +1,9 @@
-import LeafletMap from "./leaflet-map";
-
 export default class LeafletMapManager {
     init(rootNode) {
         rootNode.querySelectorAll('[data-leaflet]').forEach(async mapNode => {
+            const {default: LeafletMap} = await import(
+                /* webpackChunkName: "leaflet" */ './leaflet-map'
+            );
             const data = JSON.parse(mapNode.getAttribute('data-leaflet'));
             const leafletMap = new LeafletMap(mapNode, data);
 
