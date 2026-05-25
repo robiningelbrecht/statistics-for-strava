ef4135b7d46eea3435cb2872d85a126641d55310
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-05-25 20:37:35 +0200

    ISSUE #2072: Improve UX of Zwift dashboard widget

--- src/Domain/Zwift/ZwiftMap.php
+++ src/Domain/Zwift/ZwiftMap.php
@@ -34,7 +34,7 @@ final readonly class ZwiftMap implements LeafletMap
 
     public function getLabel(): string
     {
-        return ucfirst(str_replace('-', ' ', $this->mapName));
+        return ucwords(str_replace('-', ' ', $this->mapName));
     }
 
     public function getTileLayer(): ?string
