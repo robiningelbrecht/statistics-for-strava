072b4cdd258e8aa3776f08913f915aa8ef3646b8
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-06-03 13:48:20 +0200

    ISSUE #2123: Move manifest.json to build folder

--- src/Application/Build/BuildManifest/BuildManifestCommandHandler.php
+++ src/Application/Build/BuildManifest/BuildManifestCommandHandler.php
@@ -28,6 +28,7 @@ final readonly class BuildManifestCommandHandler implements CommandHandler
         $athlete = $this->athleteRepository->find();
 
         $manifest = file_get_contents($this->kernelProjectDir.'/templates/manifest.json');
+        assert(is_string($manifest));
         $manifest = str_replace('[APP_NAME]', sprintf('Statistics for Strava | %s', $athlete->getName()), $manifest);
         $manifest = str_replace('[APP_HOST]', (string) $this->appUrl, $manifest);
         $manifest = str_replace('[APP_BASE_PATH]', $this->appUrl->getBasePath() ?? '', $manifest);
