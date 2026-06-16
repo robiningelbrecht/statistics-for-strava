bef4faa294d5fb6d823047b16566beb54c004881
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-06-16 21:00:56 +0200

    ISSUE #2173: Navigation and sidebar for admin area

--- public/js/components/sidebar.js
+++ public/js/components/sidebar.js
@@ -2,7 +2,7 @@ import {eventBus, Events} from "../core/event-bus";
 
 export default class Sidebar {
     init() {
-        document.getElementById('toggle-sidebar-collapsed-state').addEventListener('click', () => {
+        document.getElementById('toggle-sidebar-collapsed-state')?.addEventListener('click', () => {
             const collapsed = document.documentElement.toggleAttribute('data-sidebar-collapsed');
             localStorage.setItem('sideNavCollapsed', String(collapsed));
             eventBus.emit(Events.SIDEBAR_RESIZED);
