6a17f96351b67f34dbd778b0c66724d466893acd
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-06-17 13:28:35 +0200

    ISSUE #2173: Added proper feature flags

--- src/Infrastructure/Config/FeatureFlag.php
+++ src/Infrastructure/Config/FeatureFlag.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Config;
 
+use App\Application\AppVersion;
+
 enum FeatureFlag: string
 {
     case ADMIN = 'admin';
@@ -12,6 +14,6 @@ enum FeatureFlag: string
     {
         $envVar = sprintf('FEATURE_ENABLE_%s', strtoupper($this->value));
 
-        return filter_var($_SERVER[$envVar] ?? false, FILTER_VALIDATE_BOOL);
+        return AppVersion::isAtLeastVersion5() || filter_var($_SERVER[$envVar] ?? false, FILTER_VALIDATE_BOOL);
     }
 }
