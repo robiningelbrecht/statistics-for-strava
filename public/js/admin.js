1f30a436d8145ee656e210755e13df69930b519d
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-06-17 10:29:00 +0200

    ISSUE #2173: Convert stateless init managers to functions

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,20 +1,10 @@
 import initSidebar from "./components/sidebar";
 import initPasswordToggle from "./components/password-toggle";
+import initFormLoadingState from "./components/form-loading-state";
 import {initDrawers} from "flowbite";
 
 initDrawers();
 
 initSidebar();
 initPasswordToggle();
-
-document.addEventListener('submit', function (event) {
-    const form = event.target;
-    if (!(form instanceof HTMLFormElement)) {
-        return;
-    }
-
-    form.querySelectorAll('button[data-has-loading-state]').forEach(function (button) {
-        button.classList.add('is-loading');
-        button.disabled = true;
-    });
-});
+initFormLoadingState();
