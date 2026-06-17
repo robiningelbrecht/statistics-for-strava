aa0f000f479c3ef31ddceababfb3a234b2e9dab2
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-06-17 13:20:08 +0200

    ISSUE #2173: Added proper feature flags

---
+++ src/Infrastructure/Config/FeatureFlag.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Config;
+
+enum FeatureFlag: string
+{
+    case ADMIN = 'admin';
+
+    public function isEnabled(): bool
+    {
+        $envVar = sprintf('FEATURE_ENABLE_%s', strtoupper($this->value));
+
+        return filter_var($_SERVER[$envVar] ?? false, FILTER_VALIDATE_BOOL);
+    }
+}
