61e950c6181721eff22f75af397a7fc93f5ffe9b
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-06-24 16:39:44 +0200

    ISSUE #2193: Delete activity images on activity delete

--- src/Domain/Import/FileImportRepository.php
+++ src/Domain/Import/FileImportRepository.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace App\Domain\Import;
 
+use App\Domain\Activity\ActivityId;
+
 interface FileImportRepository
 {
     public function add(FileImport $fileImport): void;
 
     public function existsForFileHash(string $fileHash): bool;
+
+    public function deleteForActivity(ActivityId $activityId): void;
 }
--- tests/Domain/Import/DbalFileImportRepositoryTest.php
+++ tests/Domain/Import/DbalFileImportRepositoryTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Domain\Import;
 
+use App\Domain\Activity\ActivityId;
 use App\Domain\Import\DbalFileImportRepository;
 use App\Domain\Import\FileImportId;
 use App\Domain\Import\FileImportRepository;
@@ -40,6 +41,41 @@ class DbalFileImportRepositoryTest extends ContainerTestCase
                 ->withFileImportId(FileImportId::fromUnprefixed('2'))
                 ->withFileHash('duplicate-hash')
                 ->build()
+        );
+    }
+
+    public function testDeleteForActivity(): void
+    {
+        $this->fileImportRepository->add(
+            FileImportBuilder::fromDefaults()
+                ->withFileImportId(FileImportId::fromUnprefixed('1'))
+                ->withFileHash('hash-1')
+                ->withActivityId(ActivityId::fromUnprefixed('1'))
+                ->build()
+        );
+        $this->fileImportRepository->add(
+            FileImportBuilder::fromDefaults()
+                ->withFileImportId(FileImportId::fromUnprefixed('2'))
+                ->withFileHash('hash-2')
+                ->withActivityId(ActivityId::fromUnprefixed('2'))
+                ->build()
+        );
+
+        $this->fileImportRepository->deleteForActivity(ActivityId::fromUnprefixed('1'));
+
+        $this->assertSame(
+            0,
+            (int) $this->getConnection()->executeQuery(
+                'SELECT COUNT(*) FROM FileImport WHERE activityId = :activityId',
+                ['activityId' => (string) ActivityId::fromUnprefixed('1')]
+            )->fetchOne()
+        );
+        $this->assertSame(
+            1,
+            (int) $this->getConnection()->executeQuery(
+                'SELECT COUNT(*) FROM FileImport WHERE activityId = :activityId',
+                ['activityId' => (string) ActivityId::fromUnprefixed('2')]
+            )->fetchOne()
         );
     }
 
