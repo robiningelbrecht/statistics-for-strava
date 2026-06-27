5a1173da424bf2d75833d74179ee7d7c5ef1afe4
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-06-27 20:02:02 +0200

    ISSUE #2200: Added delete form for gear maintenance logs

--- src/Controller/Admin/Gear/ManageGearMaintenanceLogFormRequestHandler.php
+++ src/Controller/Admin/Gear/ManageGearMaintenanceLogFormRequestHandler.php
@@ -8,6 +8,8 @@ use App\Domain\Gear\Gear;
 use App\Domain\Gear\GearRepository;
 use App\Domain\Gear\Maintenance\GearComponent;
 use App\Domain\Gear\Maintenance\Log\AddGearMaintenanceLog\AddGearMaintenanceLog;
+use App\Domain\Gear\Maintenance\Log\DeleteGearMaintenanceLog\DeleteGearMaintenanceLog;
+use App\Domain\Gear\Maintenance\Log\GearMaintenanceLog;
 use App\Domain\Gear\Maintenance\Log\GearMaintenanceLogId;
 use App\Domain\Gear\Maintenance\Log\GearMaintenanceLogRepository;
 use App\Domain\Gear\Maintenance\Log\UpdateGearMaintenanceLog\UpdateGearMaintenanceLog;
@@ -42,6 +44,38 @@ final readonly class ManageGearMaintenanceLogFormRequestHandler
     #[Route(path: '/admin/gear/maintenance-logs/{gearMaintenanceLogId}/edit', name: 'admin_edit_gear_maintenance_log', methods: ['GET'], priority: 10)]
     public function handleEdit(string $gearMaintenanceLogId): Response
     {
+        ['log' => $gearMaintenanceLog, 'gearName' => $gearName, 'componentLabel' => $componentLabel, 'taskLabel' => $taskLabel]
+            = $this->resolveActionableLog($gearMaintenanceLogId);
+
+        return new Response($this->twig->render('html/admin/page/gear/edit-gear-maintenance-log.html.twig', [
+            'dispatchCommand' => UpdateGearMaintenanceLog::NAME,
+            'gearMaintenanceLog' => $gearMaintenanceLog,
+            'componentLabel' => $componentLabel,
+            'taskLabel' => $taskLabel,
+            'gearName' => $gearName,
+        ]));
+    }
+
+    #[Route(path: '/admin/gear/maintenance-logs/{gearMaintenanceLogId}/delete', name: 'admin_delete_gear_maintenance_log', methods: ['GET'], priority: 10)]
+    public function handleDelete(string $gearMaintenanceLogId): Response
+    {
+        ['log' => $gearMaintenanceLog, 'gearName' => $gearName, 'componentLabel' => $componentLabel, 'taskLabel' => $taskLabel]
+            = $this->resolveActionableLog($gearMaintenanceLogId);
+
+        return new Response($this->twig->render('html/admin/page/gear/delete-gear-maintenance-log.html.twig', [
+            'dispatchCommand' => DeleteGearMaintenanceLog::NAME,
+            'gearMaintenanceLog' => $gearMaintenanceLog,
+            'componentLabel' => $componentLabel,
+            'taskLabel' => $taskLabel,
+            'gearName' => $gearName,
+        ]));
+    }
+
+    /**
+     * @return array{log: GearMaintenanceLog, gearName: string, componentLabel: string, taskLabel: string}
+     */
+    private function resolveActionableLog(string $gearMaintenanceLogId): array
+    {
         $gearMaintenanceLog = $this->gearMaintenanceLogRepository->find(GearMaintenanceLogId::fromString($gearMaintenanceLogId));
 
         $gearMaintenanceConfig = $this->config->loadGearMaintenance();
@@ -50,15 +84,14 @@ final readonly class ManageGearMaintenanceLogFormRequestHandler
         $gear = $this->gearRepository->findAll()->getByGearId($gearMaintenanceLog->getGearId());
 
         if (!$gear instanceof Gear || !$component instanceof GearComponent || !$task instanceof MaintenanceTask) {
-            throw new EntityNotFound(sprintf('Gear maintenance log "%s" is no longer editable', $gearMaintenanceLogId));
+            throw new EntityNotFound(sprintf('Gear maintenance log "%s" is no longer available', $gearMaintenanceLogId));
         }
 
-        return new Response($this->twig->render('html/admin/page/gear/edit-gear-maintenance-log.html.twig', [
-            'dispatchCommand' => UpdateGearMaintenanceLog::NAME,
-            'gearMaintenanceLog' => $gearMaintenanceLog,
+        return [
+            'log' => $gearMaintenanceLog,
+            'gearName' => $gear->getName(),
             'componentLabel' => (string) $component->getLabel(),
             'taskLabel' => (string) $task->getLabel(),
-            'gearName' => $gear->getName(),
-        ]));
+        ];
     }
 }
