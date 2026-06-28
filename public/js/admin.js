8214ea8671ac677637b358a8a5ee05e501e7823a
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2026-06-28 20:28:14 +0200

    ISSUE #2200: Added contextual panel to admin sidebar

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,15 +1,14 @@
-import initSidebar from "./components/sidebar";
 import initPasswordToggle from "./components/password-toggle";
 import initFormLoadingState from "./components/form-loading-state";
 import initDispatchCommandForm from "./components/dispatch-command-form";
 import initDependentSelects from "./components/dependent-select";
 import FileDropzoneUpload from "./features/file-upload/file-dropzone-upload";
 import {initImageDropZones} from "./features/file-upload/image-dropzone-upload";
-import {initDrawers} from "flowbite";
+import {initDrawers, initCollapses} from "flowbite";
 
 initDrawers();
+initCollapses();
 
-initSidebar();
 initPasswordToggle();
 initFormLoadingState();
 initDispatchCommandForm();
